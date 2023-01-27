aeb1f23034c79c8ce7d6c055d1d2fac315178ebb
Yoonjae Baek <52307613+whyjyj@users.noreply.github.com>
2023-01-27 17:29:40 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -4,7 +4,7 @@ using namespace std;
 /*
     내림차순 정렬하고 붙이면 되는데 정렬 기준 세우는 것이 관건
     맨 앞에 자리수 큰 순서대로, 길이가 더 길면 그 다음 숫자가 다음 기준 -> 사전 순?
-    그럼 3, 30은 어떻게 정렬되지? std::sort 기준 3이 앞임. 그니까 3이 작은 거
+    그럼 3, 30은 어떻게 정렬되지? std::sort 기준 3이 앞임. 그니까 3이 작은 거, 30이 큰 거
     330 vs 303 -> 330이 앞에 와야 한다(더 크다)
     + 가장 큰 수 구하고 문자열로 구하는 거니 0에 대한 처리가 필요하다
 */
@@ -17,16 +17,8 @@ string solution(vector<int> numbers) {
     
     vector<string> nums;
     string answer = "";
-    int zero_cnt = 0;
-    int numbers_size = numbers.size();
     
     for(int num: numbers) {
-        if(num==0) zero_cnt += 1;
-        if(zero_cnt == numbers_size) {
-            nums.clear();
-            nums.push_back(to_string(0));
-            break;
-        }
         string s = to_string(num);
         nums.push_back(s);
     }
@@ -36,6 +28,7 @@ string solution(vector<int> numbers) {
     for(string s: nums) {
         answer.append(s);
     }
+    if(answer[0]=='0') return "0";
     
     return answer;
 }
