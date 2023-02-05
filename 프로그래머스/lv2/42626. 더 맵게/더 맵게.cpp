b9cc8bee96062ade36d29fc115d8343a8a28077f
Yoonjae Baek <52307613+whyjyj@users.noreply.github.com>
2023-02-05 17:05:52 +0900

    [level 2] Title: 더 맵게, Time: 224.41 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.cpp
@@ -13,10 +13,10 @@ int solution(vector<int> scoville, int K) {
     while(true) {
         int top = pq.top(); pq.pop();
         if(top < K) {
-            if(pq.empty()) return -1;
+            if(pq.empty()) return -1; // 조건 만족 못 했는데 섞을 음식이 없음
             int second_top = pq.top(); pq.pop();
             int mix = top + (second_top * 2);
-            if(top == 0 && second_top == 0 && K!=0) return -1;
+            if(top == 0 && second_top == 0 && K!=0) return -1; // 섞을 음식 있지만 절대 k 못 넘음
             pq.push(mix);
             answer++;
         }
