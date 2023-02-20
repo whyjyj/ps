e6771531b603ac89a2eb2c41e286862df1587045
Yoonjae Baek <52307613+whyjyj@users.noreply.github.com>
2023-02-20 23:13:40 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,7 +1,6 @@
 -- 코드를 입력하세요
-SELECT o.animal_id, o.name
-FROM animal_outs o
-LEFT JOIN animal_ins i
-ON o.animal_id = i.animal_id
-WHERE i.animal_id is null
-ORDER BY o.animal_id
+SELECT outs.animal_id, outs.name
+FROM animal_ins ins
+RIGHT JOIN animal_outs outs ON ins.animal_id = outs.animal_id
+WHERE ins.animal_id IS NULL AND outs.animal_id IS NOT NULL
+ORDER BY 1, 2
