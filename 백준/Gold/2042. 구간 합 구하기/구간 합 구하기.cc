a88de44819a15fa98632e99aeb749cec15230433
Yoonjae Baek <52307613+whyjyj@users.noreply.github.com>
2023-07-06 16:40:34 +0900

    [Gold I] Title: 구간 합 구하기, Time: 232 ms, Memory: 41088 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int MAX_NUM = 1000000;
+
+int n, m, k;
+vector<ll> num;
+vector<ll> tree;
+
+// num: num array
+// tree: segment tree
+// node: 노드 번호
+// node에 저장되어 있는 합의 범위가 start - end
+void init(vector<long long> &num, vector<long long> &tree, int nodeNum, int nodeL, int nodeR)
+{
+    // Leaf
+    if (nodeL == nodeR)
+    {
+        tree[nodeNum] = num[nodeL];
+    }
+    // Internal Node
+    else
+    {
+        int mid = (nodeL + nodeR) / 2;
+        init(num, tree, nodeNum * 2, nodeL, mid);
+        init(num, tree, nodeNum * 2 + 1, mid + 1, nodeR);
+        tree[nodeNum] = tree[nodeNum * 2] + tree[nodeNum * 2 + 1];
+    }
+}
+
+ll sum(vector<ll> &tree, int nodeNum, int nodeL, int nodeR, int L, int R)
+{
+    if (R < nodeL || nodeR < L)
+    {
+        return 0;
+    }
+    if (L <= nodeL && nodeR <= R)
+    {
+        return tree[nodeNum];
+    }
+    int mid = (nodeL + nodeR) / 2;
+    ll lsum = sum(tree, nodeNum * 2, nodeL, mid, L, R);
+    ll rsum = sum(tree, nodeNum * 2 + 1, mid + 1, nodeR, L, R);
+    return lsum + rsum;
+}
+
+void update(vector<long long> &num, vector<long long> &tree, int node, int start, int end, int index, long long val)
+{
+    if (index < start || index > end)
+    {
+        return;
+    }
+    if (start == end)
+    {
+        num[index] = val;
+        tree[node] = val;
+        return;
+    }
+    update(num, tree, node * 2, start, (start + end) / 2, index, val);
+    update(num, tree, node * 2 + 1, (start + end) / 2 + 1, end, index, val);
+    tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m >> k;
+
+    num.resize(n);
+    tree.resize(4 * n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> num[i];
+    }
+
+    init(num, tree, 1, 0, n - 1);
+
+    for (int i = 0; i < m + k; i++)
+    {
+        int a, b;
+        ll c;
+        cin >> a >> b >> c;
+        if (a == 1)
+        {
+            // change b'th number to c
+            update(num, tree, 1, 0, n - 1, b - 1, c);
+        }
+        else if (a == 2)
+        {
+            // sum from b'th to c'th number
+            cout << sum(tree, 1, 0, n - 1, b - 1, c - 1) << '\n';
+        }
+    }
+}
