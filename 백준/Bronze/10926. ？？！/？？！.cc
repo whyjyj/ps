148f3dee3fb856559938b66f14c2ba07880a17b1
Yoonjae Baek <52307613+whyjyj@users.noreply.github.com>
2023-07-17 23:49:26 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string input;
+    cin >> input;
+    cout << input + "\?\?!";
+}
