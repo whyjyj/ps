eda826771216087302a248b2c2cd8c99e1050795
Yoonjae Baek <52307613+whyjyj@users.noreply.github.com>
2023-07-28 00:17:04 +0900

    [Silver I] Title: 절댓값 힙, Time: 16 ms, Memory: 2532 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+class cmp
+{
+public
+    :
+    bool
+    operator()(int a, int b)
+    {
+        if (abs(a) != abs(b))
+        {
+            return abs(a) > abs(b);
+        }
+        else
+            return a > 0 && b < 0;
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    priority_queue<int, vector<int>, cmp> pq;
+    int n;
+
+    cin >> n;
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        if (x == 0)
+        {
+            if (!pq.empty())
+            {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+            else
+                cout << 0 << '\n';
+        }
+        else
+        {
+            pq.push(x);
+        }
+    }
+}
